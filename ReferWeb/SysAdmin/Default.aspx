b97e4b31aab596c748e0e17f710a1b96eef75289
Paul Jones <pjones@geninfo.com>
2016-07-20 20:10:28 -0400

    Initial check in

---
+++ ReferWeb/SysAdmin/Default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="SysAdmin_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login</title>
+    <link type="text/css" href="../Content/bootstrap.min.css" rel="stylesheet" />
+    <link type="text/css" href="../Style/StyleSheet.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <table class="">
+        
+        <tr>
+            <td>Username</td>
+            <td><asp:TextBox ID="txtUser" runat="server" /></td>
+        </tr>
+        <tr>
+            <td>Password</td>
+            <td><asp:TextBox ID="txtPassword" TextMode="Password" runat="server" /></td>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td><asp:Button ID="btnLogin" Text="Login" CssClass="" runat="server"/></td>
+        </tr>
+    </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ ReferralMS/ReferralMS/Database.cs
@@ -0,0 +1,80 @@
+﻿#region Namespace References
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#endregion
+
+namespace ReferralMS
+{
+    public class Database
+    {
+        #region Members
+        string connectionString;
+        #endregion
+
+        #region Constructors
+        public Database() { }
+
+        public Database(string ConnectionString)
+        {
+            connectionString = ConnectionString;
+        }
+        #endregion
+
+        #region Methods
+
+        public int Add(SqlCommand cmd)
+        {
+            int id = 0;
+            using (SqlConnection conn = GetOpenConnection())
+            {
+                cmd.Connection = conn;
+                cmd.ExecuteScalar();
+                id = Convert.ToInt32(cmd.Parameters["@Id"].Value);
+            }
+
+            return id;
+        }
+
+        public int Update(SqlCommand cmd)
+        {
+            int id = 0;
+            using (SqlConnection conn = GetOpenConnection())
+            {
+                cmd.Connection = conn;
+                cmd.ExecuteScalar();
+                id = Convert.ToInt32(cmd.Parameters["@RowsAffected"].Value);
+            }
+
+            return id;
+        }
+
+        public DataTable Read(SqlCommand cmd)
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = GetOpenConnection())
+            {
+                cmd.Connection = conn;
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+
+            return dt;
+        }
+
+        public SqlConnection GetOpenConnection()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = connectionString;
+            conn.Open();
+
+            return conn;
+        }
+
+        #endregion
+    }
+}
